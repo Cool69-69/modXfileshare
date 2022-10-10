fcf85041649bcaf889a0d3bf4bdb51ef722e774a
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-10 11:01:52 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+import aiohttp
+
+####################  Tnlink  ####################
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+    url = f'https://tnlink.in/api'
+    params = {'api': '5cc7e548ecb43e7cd19b8c9a6d20963cd49afeb2',
+              'url': link,
+              }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+            data = await response.json()
+            if data["status"] == "success":
+                return data['shortenedUrl']
+            else:
+                return f"Error: {data['message']}"
