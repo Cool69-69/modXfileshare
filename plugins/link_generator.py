f723961b406f5692363e51b151bc26135b6bae20
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-11 09:09:31 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,8 +37,10 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     tnlink =  get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={tnlink}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{tnlink}", quote=True, reply_markup=reply_markup)
+    print (tnlink)
+    await (link = tnlink)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -57,5 +59,7 @@ async def link_generator(client: Client, message: Message):
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     tnlink = get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={tnlink}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{tnlink}", quote=True, reply_markup=reply_markup)
+    print (tnlink)
+    await (link = tnlink)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
