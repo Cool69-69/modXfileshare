6d4e1d26e0e122d792194e38f520dfc6466a41d7
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-11 23:25:34 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,11 +9,23 @@ from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
-	
+async def channel_post(client: Client, message: Message):
+    reply_text = await message.reply_text("Please Wait...!", quote = True)
+    try:
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        return
+    converted_id = post_message.id * abs(client.db_channel.id)
+    string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = await get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/targetx25')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/share/url?url={link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
 
@@ -30,7 +42,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = await get_shortlink(f"https://telegram.me/{client.username}?start={base64_string}")
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/targetx25')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://t.me/share/url?url={link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
