fe2b5aabb7df295e365030e5ebcb496af0249061
Abhay Pratap Singh <80690695+Cool69-69@users.noreply.github.com>
2022-10-11 23:39:51 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
     url = f'https://tnlink.in/api'
-    params = {'api': '1e4c9aa6433ac5fea14286254767b2139ec068a',
+    params = {'api': '75da926e3f248de287cb0a770ea77048012fa926',
               'url': link,
               }
 
